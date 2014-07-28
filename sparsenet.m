a3a4d694d64def937a5a67227b7b950dde024602
primetang <tanggefu@gmail.com>
2014-07-28 08:38:07 +0800

    update

--- sparsenet.m
+++ sparsenet.m
@@ -40,38 +40,38 @@ h=display_network(A,S_var);
 for t=1:num_trials
 
     % choose an image for this batch
-    
+
     i=ceil(num_images*rand);
     this_image=IMAGES(:,:,i);
-    
+
     % extract subimages at random from this image to make data vector X
-    
+
     for i=1:batch_size
         r=BUFF+ceil((image_size-sz-2*BUFF)*rand);
         c=BUFF+ceil((image_size-sz-2*BUFF)*rand);
         X(:,i)=reshape(this_image(r:r+sz-1,c:c+sz-1),L,1);
     end
-    
+
     % calculate coefficients for these data via conjugate gradient routine
-    
+
     S=cgf_fitS(A,X,noise_var,beta,sigma,tol);
-    
+
     % calculate residual error
-    
+
     E=X-A*S;
-    
+
     % update bases
-    
+
     dA=zeros(L,M);
     for i=1:batch_size
         dA = dA + E(:,i)*S(:,i)';
     end
     dA = dA/batch_size;
-    
+
     A = A + eta*dA;
-    
+
     % normalize bases to match desired output variance
-    
+
     for i=1:batch_size
         S_var = (1-var_eta)*S_var + var_eta*S(:,i).*S(:,i);
     end
@@ -80,11 +80,11 @@ for t=1:num_trials
     for i=1:M
         A(:,i)=gain(i)*A(:,i)/normA(i);
     end
-    
+
     % display
-    
+
     if (mod(t,display_every)==0)
         display_network(A,S_var,h);
     end
-    
+
 end
